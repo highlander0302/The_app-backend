a38d6d8e1d52748159d09092b644efee14c4b04f
TheApp Team <noreply@example.com>
2025-11-13 17:03:52 +0100

    build(catalog/admin) register models so that they are visable in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,126 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from .models import (
+    ActionCamera,
+    Desktop,
+    DigitalCamera,
+    DigitalPen,
+    Drone,
+    EReader,
+    FitnessTracker,
+    GamingConsole,
+    HandheldConsole,
+    Headphones,
+    Laptop,
+    Modem,
+    Projector,
+    Router,
+    Smartphone,
+    SmartSpeaker,
+    Smartwatch,
+    Tablet,
+    VRGamingSystem,
+    WearableMedicalDevice,
+)
+
+
+@admin.register(Laptop)
+class LaptopAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Desktop)
+class DesktopAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Tablet)
+class TabletAdmin(ModelAdmin):
+    pass
+
+
+# Appliances
+@admin.register(Smartphone)
+class SmartphoneAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Smartwatch)
+class SmartwatchAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(FitnessTracker)
+class FitnessTrackerAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Headphones)
+class HeadphonesAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(SmartSpeaker)
+class SmartSpeakerAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(DigitalCamera)
+class DigitalCameraAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(ActionCamera)
+class ActionCameraAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Drone)
+class DroneAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(GamingConsole)
+class GamingConsoleAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(HandheldConsole)
+class HandheldConsoleAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(VRGamingSystem)
+class VRGamingSystemAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Router)
+class RouterAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Modem)
+class ModemAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(EReader)
+class EReaderAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(Projector)
+class ProjectorAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(WearableMedicalDevice)
+class WearableMedicalDeviceAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(DigitalPen)
+class DigitalPenAdmin(ModelAdmin):
+    pass
